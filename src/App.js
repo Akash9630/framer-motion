c059e5374b272a6fd0e7868e24c2a6a1b25b7319
Akash_96 <realakash9630@gmail.com>
2022-12-29 23:34:05 +0530

    drop in

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
+import { useState } from "react";
+import Modal from "./components/modal";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+
   return (
     <div>
-      {/* <p style={{ color: "blue" }}>hi..</p> */}
       <motion.button
-        className="save-button"
-        onClick={() => null}
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
-      >Launch modal</motion.button>
+        className="save-button"
+        onClick={() => (modalOpen ? close() : open())}
+      >
+        Launch modal
+      </motion.button>
+      <AnimatePresence
+        // Disable any initial animations on children that
+        // are present when the component is first rendered
+        initial={false}
+        // Only render one component at a time.
+        // The exiting component will finish its exit
+        // animation before entering component is rendered
+        exitBeforeEnter={true}
+        // Fires when all exiting nodes have completed animating out
+        onExitComplete={() => null}
+      >
+        {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
+      </AnimatePresence>
     </div>
-  );
+  )
 }
 
 export default App;
